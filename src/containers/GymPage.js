96b23274c2f10f9c16f50369741fd231456c0ebc
Ross Jones <rossmosis.jones@gmail.com>
2019-05-23 12:34:01 -0400

    added local gym markers using google places

--- src/containers/GymPage.js
+++ src/containers/GymPage.js
@@ -1,9 +1,11 @@
+/* global google */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { mapsAPI } from '../constants/API'
 import { Layout, Button } from 'antd'
-import { compose, withProps } from "recompose"
+import { compose, withProps, withHandlers, withState } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { SearchBox } from "react-google-maps/lib/components/places/SearchBox"
 
 const { Content } = Layout
 
@@ -15,13 +17,50 @@ const MyMapComponent = compose(
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
-  withGoogleMap
+  withGoogleMap,
+  withState('places', 'updatePlaces', ''),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    }
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref
+      },
+      fetchPlaces: ({ updatePlaces }) => () => {
+        let places
+        const bounds = refs.map.getBounds()
+        const service = new google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
+        const request = {
+          bounds: bounds,
+          type: ['gym']
+        }
+        service.nearbySearch(request, (results, status) => {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results)
+            updatePlaces(results)
+          }
+        })
+      }
+    }
+  }),
 )((props) =>
   <GoogleMap
+    onTilesLoaded={props.fetchPlaces}
+    ref={props.onMapMounted}
+    onBoundsChanged={props.fetchPlaces}
     defaultZoom={12}
     defaultCenter={{ lat: props.latitude, lng: props.longitude }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: props.latitude, lng: props.longitude }} onClick={props.onMarkerClick} />}
+    {console.log(props)}
+    {props.places && props.places.map((place, i) =>
+      <Marker 
+        key={i} 
+        position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} 
+        icon={require('../images/iconfinder_gym_38044.png')}  
+      />
+    )}
   </GoogleMap>
 )
 
