bccee31a2a5ddd222c386f0c663782e90060fdd6
Ross Jones <rossmosis.jones@gmail.com>
2019-07-30 01:19:10 -0400

    making api for global use

--- src/containers/GymPage.js
+++ src/containers/GymPage.js
@@ -15,7 +15,7 @@ const { Text } = Typography
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${mapsAPI}&v=3.exp&libraries=geometry,drawing,places`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${MAPS_API}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: '100%' }} />,
     containerElement: <div style={{ height: '85vh', width: '98%' }} />,
     mapElement: <div style={{ height: '100%' }} />,
